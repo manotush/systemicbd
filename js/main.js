fbf1e1ccf00199a98a554c6f912e605c4422be63
manotush <mk747294@gmail.com>
2021-01-05 00:14:53 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*slider area*/
+$(document).ready(function(){
+    $('.slider_area').bxSlider();
+    $('#main_menu').meanmenu({
+      meanmenuContainer: '#mobile_menu',
+      meanScreenWidth:768,
+    });
+  });
+ 
